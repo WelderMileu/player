10c91bbd2b658fe6c76c2044696953213755904d
weldermileu <welderfelix21@gmail.com>
2021-08-15 19:57:18 -0300

    alterando toda a midia

--- script.js
+++ script.js
@@ -1,52 +1,26 @@
 const $ = e => document.querySelector(e);
 
-let pathImage = 'assets/albums/';
-let pathMusic = 'assets/audio/';
-
-let musics = [
-	{
-		name: "ID2 - Aos Pés da Cruz",
-		image: pathImage + "Aos_Pés_da_Cruz.jpg",
-		music: pathMusic + "Aos_Pés_da_Cruz.mp3"
-	},
-	{
-		name: "Avicii - Never Leave Me ft. Joe Janiak",
-		image: pathImage + "Never_Leave_Me_ft_Joe_Janiak.jpg",
-		music: pathMusic + "Never_Leave_Me_ft_Joe_Janiak.mp3"
-	},
-	{
-		name: "Avicii - Bad Reputation ft Joe. Janiak",
-		image: pathImage + "Never_Leave_Me_ft_Joe_Janiak.jpg",
-		music: pathMusic + "Bad_Reputation_ft_Joe_Janiak.mp3"
-	},
-	{
-		name: "Avicii - The Days (Subtitulada al Español)",
-		image: pathImage + "The_days.jpg",
-		music: pathMusic + "The_days.mp3"
-	},
-	{
-		name: "Legends Never Die",
-		image: pathImage + "Legends_Never_Die.jpg",
-		music: pathMusic + "Legends_Never_Die.mp3"
-	}
-];	
+let pathImage = 'assets/images/';
+let pathAudio = 'assets/audio/';
 
 let initial = 0;
 let position;
 let playerPlay = false;
 
+let media = []
+
 const actions = {
 	insertMusic(initial) {
-		$('#album-image').src = musics[initial].image;
-		$('.music-title').innerHTML = musics[initial].name;
-		$('#actions-audio').src = musics[initial].music;
+		$('#album-image').src = media[initial].image;
+		$('.music-title').innerHTML = media[initial].name;
+		$('#actions-audio').src = media[initial].audio;
+		$('#download-link').href = media[initial].audio;
+
 		actions.play()
-	
-		$('#download-link').href = musics[initial].music;
 	},
 
 	next() {
-		if(initial < musics.length - 1) {
+		if(initial < media.length - 1) {
 			initial++;
 			actions.insertMusic(initial);
 		} else {
@@ -72,7 +46,7 @@ const actions = {
 	},
 
 	musicPosition() {
-		$('.position').innerHTML = `${initial + 1}/${musics.length}`;
+		$('.position').innerHTML = `${initial + 1}/${media.length}`;
 	},
 
 	play() {
@@ -121,7 +95,29 @@ const actions = {
 	}
 };
 
-actions.insertMusic(initial);
+fetch('./data.json')
+	.then(elem => elem.json())
+	.then(data => {
+
+		data.map(elem => {
+			try {
+				let dados = {
+					"name" : elem.name,
+					"image" : pathImage + elem.image,
+					"audio" : pathAudio + elem.audio
+				}
+
+			 	media.push(dados);
+			} catch (e) {
+				console.log("my error and:" + e)
+			}
+
+		});
+		
+		actions.insertMusic(initial);
+
+	}).catch(err => console.log(err));
+
 actions.musicPosition();
 actions.timeMusic();
 actions.volumeAudio();
